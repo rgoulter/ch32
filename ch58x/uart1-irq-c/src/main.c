ddc7aecfd2d854d181f4edaec39c7439260f0762
Richard Goulter <richard.goulter@gmail.com>
2024-08-16 09:55:41 +0700

    ch58x: copy UART1, use IRQ

---
+++ ch58x/uart1-irq-c/src/main.c
@@ -0,0 +1,76 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : Main.c
+ * Author             : WCH
+ * Version            : V1.0
+ * Date               : 2020/08/06
+ * Description        : UART1 Demo with interrupts
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for 
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
+#include "CH58x_common.h"
+
+uint8_t TxBuff[] = "This is a tx exam\r\n";
+uint8_t RxBuff[100];
+uint8_t trigB;
+
+int main()
+{
+    uint8_t len;
+
+    SetSysClock(CLK_SOURCE_PLL_60MHz);
+
+    GPIOA_SetBits(GPIO_Pin_9);
+    GPIOA_ModeCfg(GPIO_Pin_8, GPIO_ModeIN_PU);
+    GPIOA_ModeCfg(GPIO_Pin_9, GPIO_ModeOut_PP_5mA);
+    UART1_DefInit();
+
+    UART1_SendString(TxBuff, sizeof(TxBuff));
+
+    UART1_ByteTrigCfg(UART_7BYTE_TRIG);
+    trigB = 7;
+    UART1_INTCfg(ENABLE, RB_IER_RECV_RDY | RB_IER_LINE_STAT);
+    PFIC_EnableIRQ(UART1_IRQn);
+
+    while(1);
+}
+
+__INTERRUPT
+__HIGH_CODE
+void UART1_IRQHandler(void)
+{
+    volatile uint8_t i;
+
+    switch(UART1_GetITFlag())
+    {
+        case UART_II_LINE_STAT: // Line Status error
+        {
+            UART1_GetLinSTA();
+            break;
+        }
+
+        case UART_II_RECV_RDY:
+            for(i = 0; i != trigB; i++)
+            {
+                RxBuff[i] = UART1_RecvByte();
+                UART1_SendByte(RxBuff[i]);
+            }
+            break;
+
+        case UART_II_RECV_TOUT:
+            i = UART1_RecvString(RxBuff);
+            UART1_SendString(RxBuff, i);
+            break;
+
+        case UART_II_THR_EMPTY:
+            break;
+
+        case UART_II_MODEM_CHG:
+            break;
+
+        default:
+            break;
+    }
+}
