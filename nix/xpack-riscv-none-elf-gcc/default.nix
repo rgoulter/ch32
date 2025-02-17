ae3a2599ef3238ca161db89c036d00381b09ef26
Richard Goulter <richard.goulter@gmail.com>
2025-02-17 08:24:24 +0700

    nix: xpack: add autoPatchelfHook

--- nix/xpack-riscv-none-elf-gcc/default.nix
+++ nix/xpack-riscv-none-elf-gcc/default.nix
@@ -1,4 +1,5 @@
 { lib
+, auto-patchelf
 , stdenv
 , fetchurl
 , system
@@ -24,7 +25,7 @@ let
   url = "https://github.com/xpack-dev-tools/riscv-none-elf-gcc-xpack/releases/download/v${version}/xpack-riscv-none-elf-gcc-${version}-${urls."${system}".os}.tar.gz";
 in
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "gcc-risc-v-embedded";
   version = "14.2.0-3";
 
@@ -39,8 +40,37 @@ stdenv.mkDerivation rec {
 
   noAuditTmpdir = true;
 
+  # patch elf: ignore the libraries
+  # (xpack provides these in libexec)
   installPhase = ''
     mkdir -p $out/
     cp -r . $out
-  '';
+    LIBS=$(cd libexec/ && ls lib*)
+  '' + (lib.strings.optionalString stdenv.isLinux ''
+    ${auto-patchelf}/bin/auto-patchelf \
+      --ignore-missing \
+          libcrypt.so.2 \
+          libcrypto.so.3 \
+          libexpat.so.1 \
+          libffi.so.8 \
+          libfl.so.2 \
+          libgcc_s.so.1 \
+          libgmp.so.10 \
+          libiconv.so.2 \
+          libisl.so.23 \
+          liblzma.so.5 \
+          libmpc.so.3 \
+          libmpfr.so.6 \
+          libncurses.so.6 \
+          libnsl.so.1 \
+          libpanel.so.6 \
+          libpython3.12.so.1.0 \
+          libreadline.so.8 \
+          libsqlite3.so.0 \
+          libssl.so.3 \
+          libstdc++.so.6 \
+          libz.so.1 \
+          libzstd.so.1 \
+      --paths $out/bin/
+  '');
 }
