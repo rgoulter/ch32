97ca752f7c2fd50c1e01a11f5ca35d15bc44c3a9
Richard Goulter <richard.goulter@gmail.com>
2025-03-29 15:50:41 +0700

    nix: wchisp: update cargoHash

--- nix/wchisp/default.nix
+++ nix/wchisp/default.nix
@@ -11,5 +11,5 @@ rustPlatform.buildRustPackage rec {
     rev = "c47962d214fe3d44b47d8be989eb94990ac4081f";
     hash = "sha256-Bd51ztuhp1HiW8Edq8bGjRvXfr/zlYsanKqJxj2ujk8=";
   };
-  cargoHash = "sha256-6v66yJZVtMdFNRIKNhf7aYalHGJvbNrslq46woAI7GQ=";
+  cargoHash = "sha256-RFnVz28ZPNh74Hc1nr+lGdZGDB01G7ZieHB8qzcUeWQ=";
 }
